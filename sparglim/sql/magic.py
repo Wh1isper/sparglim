874185030483081e0eeea181555ad268c2038343
Wh1isper <9573586@qq.com>
2023-07-12 18:22:29 +0800

    Merge magic function to one

--- sparglim/sql/magic.py
+++ sparglim/sql/magic.py
@@ -1,7 +1,7 @@
 #  Copyright (c) 2023 Wh1isper
 #  Licensed under the BSD 3-Clause
 
-from typing import List
+from typing import List, Optional
 
 from IPython.core.magic import Magics, cell_magic, line_magic, magics_class
 from pyspark.sql import SparkSession
@@ -21,9 +21,10 @@ class SparkMagic(Magics):
     def spark(self) -> SparkSession:
         return self.builder.get_or_create()
 
-    def format_sql(self, *sql_lines: List[str]):
+    def format_sql(self, sql_lines: List[Optional[str]]):
         lines = ""
         for line in sql_lines:
+            line = line or ""
             lines += line.strip() + "\n"
 
         return lines.strip()
@@ -32,11 +33,8 @@ class SparkMagic(Magics):
         return self.spark.sql(sql)
 
     @line_magic
-    def sql(self, line: str):
-        return self._execute(self.format_sql([line]))
-
     @cell_magic
-    def csql(self, line: str, cell: str):
+    def sql(self, line: Optional[str] = None, cell: Optional[str] = None):
         return self._execute(self.format_sql([line, cell]))
 
     def clear(self):
