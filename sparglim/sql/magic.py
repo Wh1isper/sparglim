b4f2a12c8796ccac04c289c0dbf02f9ff84f6c61
Wh1isper <9573586@qq.com>
2023-07-27 10:46:51 +0800

    Fix docs

--- sparglim/sql/magic.py
+++ sparglim/sql/magic.py
@@ -28,11 +28,16 @@ class SparkMagic(Magics):
         from datetime import datetime, date
         from pyspark.sql import Row
 
+        from sparglim.config.builder import ConfigBuilder
+        spark = ConfigBuilder().get_or_create()
+
+
         df = spark.createDataFrame([
             Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
             Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
             Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
         ])
+        df.show()
         df.createOrReplaceTempView("tb")
         ```
 
